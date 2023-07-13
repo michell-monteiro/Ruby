1015d2ffb6837786d795ac43a16e70ff67cf28c3
michell-monteiro <michellchristofferson@gmail.com>
2023-07-12 21:45:40 -0300

    Exercício 17 reajustado

--- Quarta-feira(12-07)/ex_17.rb
+++ Quarta-feira(12-07)/ex_17.rb
@@ -1,9 +1,9 @@
 # 17- Utilizando um loop until, exiba números de 1 a 20 e, ao chegar em um número divisível
 # por 4, utilize o comando redo para repetir a iteração
 
-num = 1
+num = 0
 until num > 20 do
-    puts num
-    redo if num % 4 == 0
     num += 1
+    redo if num % 4 == 0
+    puts num
 end
